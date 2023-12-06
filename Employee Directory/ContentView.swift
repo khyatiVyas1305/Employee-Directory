fe42ddfd0b29eb1da76992fbee1444a5c649fa09
khyatiVyas1305 <khyativyas1305@gmail.com>
2023-12-06 15:37:06 -0500

    Initial Commit

---
+++ Employee Directory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Employee Directory
+//
+//  Created by Khyati Vyas on 2023-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Employee Directory/Employee_DirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Employee_DirectoryApp.swift
+//  Employee Directory
+//
+//  Created by Khyati Vyas on 2023-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Employee_DirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
