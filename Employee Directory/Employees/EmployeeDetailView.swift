25c5fffc14b228ccb727a40a992c39e661dccfc6
khyatiVyas1305 <khyativyas1305@gmail.com>
2023-12-07 22:20:57 -0500

    Details View Added.

---
+++ Employee Directory/Employees/EmployeeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  EmployeeDetailView.swift
+//  Employee Directory
+//
+//  Created by Khyati Vyas on 2023-12-07.
+//
+
+import SwiftUI
+
+struct EmployeeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmployeeDetailView()
+}
