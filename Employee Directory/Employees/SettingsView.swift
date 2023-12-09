7520508bc6bfdbca4fb32ac4cde827145da4d37b
khyatiVyas1305 <khyativyas1305@gmail.com>
2023-12-08 23:08:33 -0500

    Tab View Added.

---
+++ Employee Directory/Employees/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Employee Directory
+//
+//  Created by Khyati Vyas on 2023-12-08.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
